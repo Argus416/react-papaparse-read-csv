fd6ecd5b877bf02b3edf79fa2cbf91c415d46d2c
Mohamad AL KHATIB <alkhatib.m804@gmail.com>
2023-12-22 10:43:56 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import Papa from 'papaparse';
+
+function App() {
+	const [count, setCount] = useState(0);
+	useEffect(() => {
+		fetch('./logements-et-logements-sociaux-dans-les-departements.csv')
+			.then((response) => response.text())
+			.then((text) => {
+				const data = Papa.parse(text, { header: true }).data;
+				console.log(data);
+			});
+	}, []);
+
+	return (
+		<>
+			<div>
+				<a
+					href='https://vitejs.dev'
+					target='_blank'
+				>
+					<img
+						src={viteLogo}
+						className='logo'
+						alt='Vite logo'
+					/>
+				</a>
+				<a
+					href='https://react.dev'
+					target='_blank'
+				>
+					<img
+						src={reactLogo}
+						className='logo react'
+						alt='React logo'
+					/>
+				</a>
+			</div>
+			<h1>Vite + React</h1>
+			<div className='card'>
+				<button onClick={() => setCount((count) => count + 1)}>
+					count is {count}
+				</button>
+				<p>
+					Edit <code>src/App.tsx</code> and save to test HMR
+				</p>
+			</div>
+			<p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+		</>
+	);
+}
+
+export default App;
